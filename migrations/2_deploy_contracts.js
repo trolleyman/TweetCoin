32e386403684c59ebad887645759cf0679f3ce90
Callum Tolley <cgtrolley@gmail.com>
2017-09-27 15:10:03 +0100

    Removed all ethereum files

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var Owned = artifacts.require("./Owned.sol");
-var TweetCoin = artifacts.require("./TweetCoin.sol");
-
-module.exports = function(deployer) {
-	deployer.deploy(Owned);
-	deployer.link(Owned, TweetCoin);
-	deployer.deploy(TweetCoin);
-};
